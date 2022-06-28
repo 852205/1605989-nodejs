6fae4336f8c7402b82c57efa809dc989848fadb7
Carla Cannes <carlacannes@gmail.com>
2022-06-27 22:07:19 -0300

    nodemon

---
+++ aula04/server-nodemon/server.js
@@ -0,0 +1,19 @@
+import { createServer } from 'http'
+
+
+createServer((req, res) => {
+
+    res.writeHead(200, { 'Content-type' : 'text/html; charset=utf-8'})
+
+    res.write('Bem Vindo Carla')
+
+
+
+    
+    res.end()
+}).listen(3030, () => {
+    console.log('Running server!')
+})
+
+ 
+
